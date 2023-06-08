cae53b494b529f69ceda5b24f8def9c8ec5a8e94
Nikodem Kirsz <nikodem.kirsz@gmail.com>
2023-06-08 12:17:56 +0200

    Added grpc client

---
+++ client/grpc_client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	pb "github.com/nikodem-kirsz/svc-users/api/proto/users"
+)
+
+var (
+	serverAddr = flag.String("addr", "localhost:3000", "The server addres in the form of host:port")
+)
+
+func main() {
+	flag.Parse()
+	// var opts []grpc.DialOption
+	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewUsersClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.GetAllUsers(ctx, &emptypb.Empty{})
+	if err != nil {
+		log.Fatalf("Could not get all users from grpc server: %v", err)
+	}
+	log.Printf("All users: %v", r.GetUsers())
+
+}
